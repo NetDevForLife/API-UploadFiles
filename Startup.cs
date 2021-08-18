5610596f35b14d2419f7fe661ce0ea1bcc923dbf
Angelo Pirola <angelo@aepserver.it>
2021-08-18 16:08:16 +0200

    Configurata API per upload files

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
+using API_UploadFiles.Models.Services.Application;
+using API_UploadFiles.Models.Services.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace API_UploadFiles
@@ -30,10 +25,10 @@ namespace API_UploadFiles
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_SendEmail", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_UploadFiles", Version = "v1" });
             });
 
-            // Options
+            services.AddTransient<IUploadFilesService, UploadFilesService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,7 +37,7 @@ namespace API_UploadFiles
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_SendEmail v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_UploadFiles v1"));
             }
 
             CultureInfo appCulture = new("it-IT");
